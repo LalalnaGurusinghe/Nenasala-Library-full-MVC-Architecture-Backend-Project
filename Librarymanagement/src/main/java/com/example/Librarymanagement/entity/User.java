3dabc272b28b202f3cc8810c1f6b5d5f19438d03
LalanaGurusinghe <lalanagurusinghe@gmail.com>
2025-08-01 11:12:17 +0530

    design entity part

---
+++ Librarymanagement/src/main/java/com/example/Librarymanagement/entity/User.java
@@ -0,0 +1,22 @@
+package com.example.Librarymanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles;
+}
